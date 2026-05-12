ac9e1fcf377c7c089b2d05e4fbb59dc3ceb58702
Phrilly <66703620+Phrilly@users.noreply.github.com>
2026-05-12 10:51:56 +0100

    Create config.php

---
+++ config.php
@@ -0,0 +1,10 @@
+<?php
+// config.php
+// WARNING: Do not upload this file to GitHub!
+
+// Hostinger Database Configuration
+$host = 'localhost'; 
+$dbname = 'u271511030_word_square'; 
+$user = 'Phrilly';          
+$pass = 'Np1cardyt!';   // Enter your exact MySQL Password here
+?>
