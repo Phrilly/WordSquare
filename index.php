3251e16e6733967dad8ca9a1385603083b5e6a5a
Phrilly <pirip.dunne@gmail.com>
2026-08-26 00:11:54 +0100

    Update visible app version

--- index.php
+++ index.php
@@ -332,7 +332,7 @@ $leaderboardHeading = $isCommonDay ? "TODAY'S MFD HIGH SCORES" : "TODAY'S HIGH S
   </div>
   <?php endif; ?>
 
-  <div class="version-tag">Version Dynamic Auto-Versioning</div>
+  <div class="version-tag">Version 1.3.2</div>
 
   <script src="<?= autoVer('js/state.js') ?>" defer></script>
   <script src="<?= autoVer('js/utils.js') ?>" defer></script>
